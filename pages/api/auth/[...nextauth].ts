162ef0542ee1935a8ae3508dd495c8395b729a0a
Ashutosh Kumar <crownashutosh01@gmail.com>
2021-04-04 13:52:40 +0530

    Fixed next-auth type issue
    added types from github:typedashutosh/next-auth-types#main

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import NextAuth from 'next-auth'
+import NextAuth, { Session, User } from 'next-auth'
+import { JWT } from 'next-auth/jwt'
 import Providers from 'next-auth/providers'
-import UserModel, { IUser } from '../../../Models/User.model'
+import { WithAdditionalParams } from 'next-auth/_utils'
+import UserModel from '../../../Models/User.model'
 import dbConnect from '../../../utils/dbConnect'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -22,17 +24,22 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
               placeholder: 'password'
             }
           },
-          async authorize(credentials): Promise<IUser | null> {
+          authorize: async ({
+            csrfToken,
+            username,
+            password
+          }: Record<string, string>): Promise<User | null> => {
+            if (!csrfToken) return null
             dbConnect()
-            const user = await UserModel.findOne(
+            const user: User = await UserModel.findOne(
               {
-                username: credentials.username,
-                password: credentials.password
+                username: username,
+                password: password
               },
               ['id', 'firstname', 'lastname', 'username']
             )
             // console.log(user)
-            return !!user.id ? user : null
+            return !!user._id ? user : null
           }
         })
       ],
@@ -51,22 +58,59 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         encryptionKey: process.env.JWT_ENCRYPTION_KEY
       },
       callbacks: {
-        async signIn(user, account, profile): Promise<string | boolean> {
-          // console.log({ user, account, profile })
+        signIn: async (user, account, profile): Promise<string | boolean> => {
+          //- user is returned from authorize
+          //- account is probably credential type from provider
+          //- profile is form data returned from client
+
+          // console.log('signin here:::', { user, account, profile })
           return !!user
         },
-        async redirect(url, baseUrl) {
+        redirect: async (url, baseUrl) => {
+          //- url is previous url
+          //- baseUrl is base URL,  homepage?
+
           // console.log({ url, baseUrl })
-          return url
+          return url.startsWith(baseUrl) ? url : baseUrl
         },
-        async session(session, user) {
-          session.user = user
+        session: async (
+          session: Session,
+          userOrToken: User | JWT
+        ): Promise<WithAdditionalParams<Session>> => {
+          const returnSession: WithAdditionalParams<Session> = {
+            expires: session.expires,
+            user: {
+              _id: userOrToken._id,
+              firstname: userOrToken.firstname,
+              lastname: userOrToken.lastname,
+              username: userOrToken.username
+            },
+            accessToken: session.accessToken
+          }
           // console.log({ session, user })
-          return session
+          return returnSession
         },
-        async jwt(token, user, account, profile, isNewUser) {
-          // console.log({ token })
-          user && (token.user = user)
+        jwt: async (
+          token: JWT,
+          user: User,
+          account: Record<string, unknown>,
+          profile: Record<string, unknown>,
+          isNewUser: boolean
+        ): Promise<WithAdditionalParams<JWT>> => {
+          //-   token: this is jwt token,
+          //-   user: user returned from authorize ,
+          //-   account: type of account, here credentials
+          //?   profile: this is equal to user. I wonder why?
+          //-   isNewUser: returns boolean
+
+          // console.log({ token, user, account, profile, isNewUser })
+          user &&
+            (token = {
+              _id: user._id,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              username: user.username
+            })
           return token
         }
       },
