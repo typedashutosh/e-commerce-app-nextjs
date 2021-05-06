a0917649688607b07435ccdce7350469ba02df84
Ashutosh Kumar <68853189+typedashutosh@users.noreply.github.com>
2021-05-07 00:06:46 +0530

    its working

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -8,35 +8,37 @@ import Footer from './Footer'
 import Header from './Header'
 
 interface ILayout {
-  children: ReactElement
-  auth: boolean
+	children: ReactElement
+	session: Session | null
 }
 const useStyles = makeStyles({
-  layout: {},
-  header: {},
-  children: {},
-  footer: {}
+	layout: {},
+	header: {},
+	children: {},
+	footer: {}
 })
-const Layout: FC<ILayout> = ({ children, auth }): ReactElement => {
-  const classes = useStyles()
-  let [session, loading]: [Session | null | undefined, boolean] = useSession()
-  const [HeaderElement, setHeaderElement] = useState<ReactElement>(
-    <Header session={session} />
-  )
-  useEffect(() => {
-    getSession().then((session) => {
-      setHeaderElement(<Header session={session} />)
-    })
-  }, [auth])
-  return (
-    <div className={classes.layout}>
-      <div className={classes.header}>{HeaderElement}</div>
-      <div className={classes.children}>{children}</div>
-      <div className={classes.footer}>
-        <Footer />
-      </div>
-    </div>
-  )
+const Layout: FC<ILayout> = ({ children, session }): ReactElement => {
+	const classes = useStyles()
+	// let [session, loading]: [Session | null | undefined, boolean] = useSession()
+	// const [HeaderElement, setHeaderElement] = useState<ReactElement>(
+	//   <Header session={session} />
+	// )
+	// useEffect(() => {
+	//   getSession().then((session) => {
+	//     setHeaderElement(<Header session={session} />)
+	//   })
+	// }, [auth])
+	return (
+		<div className={classes.layout}>
+			<div className={classes.header}>
+				<Header session={session} />
+			</div>
+			<div className={classes.children}>{children}</div>
+			<div className={classes.footer}>
+				<Footer />
+			</div>
+		</div>
+	)
 }
 
 export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../Styles/global.css'
 
-import { NextPage } from 'next'
+import { NextPage, NextPageContext } from 'next'
 import { Provider, getSession } from 'next-auth/client'
 import { AppProps } from 'next/app'
 import { ReactElement, useEffect } from 'react'
@@ -9,37 +9,37 @@ import { CssBaseline, ThemeProvider } from '@material-ui/core'
 
 import Layout from '../Components/Layout'
 import theme from '../utils/theme'
-
-interface I_app extends AppProps {}
-
-const _app: NextPage<I_app> = ({
-  Component,
-  pageProps,
-  router
-}): ReactElement => {
-  useEffect(() => {
-    const jssStyles = document?.querySelector('#jss-server-side')
-    jssStyles?.parentElement?.removeChild(jssStyles)
-  }, [])
-  //  useEffect(() => {
-  //    console.log(router.query.auth)
-  //  }, [Component])
-  return (
-    <Provider session={pageProps.session}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Layout auth={Boolean(router.query.auth) || false}>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </Provider>
-  )
+import { Session } from 'next-auth'
+interface I_app_initial_props {
+	session: Session | null
 }
 
-//_app.getInitialProps = async (context: NextPageContext) => {
-//  const session = await getSession(context)
-//
-//  return { session }
-//}
-//
+interface I_app extends I_app_initial_props, AppProps {}
+
+const _app: NextPage<I_app, I_app_initial_props> = ({
+	Component,
+	pageProps,
+	session
+}): ReactElement => {
+	useEffect(() => {
+		const jssStyles = document?.querySelector('#jss-server-side')
+		jssStyles?.parentElement?.removeChild(jssStyles)
+	}, [])
+	return (
+		// <Provider session={pageProps.session}>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Layout session={session}>
+				<Component {...pageProps} />
+			</Layout>
+		</ThemeProvider>
+		// </Provider>
+	)
+}
+
+_app.getInitialProps = async (context: NextPageContext) => {
+	console.log(true)
+	return { session: await getSession(context) }
+}
+
 export default _app
